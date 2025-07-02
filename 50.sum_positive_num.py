e7a4aa7bd6f48575a30b7ec470c0cd407f40cf44
Amirul <muhdamirulaiman07@gmail.com>
2025-07-02 18:35:43 +0800

    update no.50

--- 50.sum_positive_num.py
+++ 50.sum_positive_num.py
@@ -2,6 +2,7 @@
 # negative number is entered, and prints the sum of the positive
 # numbers.
 
+# Solution 1
 
 # initialize varible 
 sum_positive = 0
@@ -17,4 +18,23 @@ while True:
         break
 
 # display the sum of positive numbers
+print(f"Sum of positive numbers: {sum_positive}")
+
+
+
+# Solution 2: without using break
+
+# initialize variable
+sum_positive = 0
+number = 0
+
+# read numbers from the user untill a negative number is entered
+while number >= 0:
+    number = int(input("Enter a number (negative number to exit): "))
+
+    # check if the number is positive
+    if number >= 0:
+        sum_positive += number
+
+# display the sum positive numbers
 print(f"Sum of positive numbers: {sum_positive}")
