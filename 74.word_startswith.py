749816ad167dfbe202bd1d54f421250c41296d08
Amirul <muhdamirulaiman07@gmail.com>
2025-07-15 14:52:09 +0800

    Add program to check if a name starts with the letter 'A'

---
+++ 74.word_startswith.py
@@ -0,0 +1,10 @@
+# Write a program that receives a name and checks that it
+# starts with the letter "A".
+
+
+name = input("Enter a name: ")
+
+if name.startswith('A'):
+    print("The name start with 'A'.")
+else:
+    print("The name does not start with 'A'.")
