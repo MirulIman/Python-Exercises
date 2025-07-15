4da7233c42f2d110987d95806004b928cdfd777a
Amirul <muhdamirulaiman07@gmail.com>
2025-07-15 14:50:33 +0800

    Add program to concatenate two words and display the result

---
+++ 71.word_concatenate.py
@@ -0,0 +1,10 @@
+# Create a program that reads two words and concatenates
+# them, displaying the resulting word.
+
+
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+
+result = word1 + word2
+
+print(f"Concatenated word: {result}" )
